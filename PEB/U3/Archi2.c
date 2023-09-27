a9eb5927c3522609994a4714a83e22b1faa9c238
Sebastián Martínez <sebymartinez70@gmail.com>
2023-09-27 19:29:15 -0300

    Create Archi2.c

---
+++ PEB/U3/Archi2.c
@@ -0,0 +1,21 @@
+/*
+Un Banco tiene un archivo secuencial SALDANTE ordenado por 
+numero de cliente con los saldos de sus casi mas de 2mil clientes
+de caja de ahorro al inicio del mes.
+cada estructura contiene : 
+    numero de cuenta de ahorro 
+    apellido-nombre
+    saldo.
+a continuacion se ingresa los siguientes datos con los movimientos del mes:
+    numero de cuenta de ahorro
+    codigo de operacion (1 deposito / 2 extraccion)
+    importe
+
+    Confeccionar un programa que 
+        a)Actualice el archivo saldante con los movieminetos registrados
+        b) informe las cuentas que en el mes no han tenido movimiento
+        con sus respectivos saldos
+        c) informe las cuentas que en el mes han realizado mas de 5 
+        extracciones
+
+*/
