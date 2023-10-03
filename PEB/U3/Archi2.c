acc9102db76467d0dc59fe9cbe9e18d3cf9de713
Sebastián Martínez <70720135+AS1508@users.noreply.github.com>
2023-10-03 09:27:37 -0300

    Update Archi2.c

--- PEB/U3/Archi2.c
+++ PEB/U3/Archi2.c
@@ -46,7 +46,7 @@ int main(){
                 vec[x].saldo+=imp;
                 vd[x]++;
             }else{
-                vec[x].saldo=imp;
+                vec[x].saldo-=imp;
                 vc[2000];
             }
             printf("numero cliente: ");
@@ -111,25 +111,3 @@ void actualizar(struct datos V[], int cantidad){
     }
     fclose(archivo);
 }
-
-/*
-Un Banco tiene un archivo secuencial SALDANTE ordenado por 
-numero de cliente con los saldos de sus casi mas de 2mil clientes
-de caja de ahorro al inicio del mes.
-cada estructura contiene : 
-    numero de cuenta de ahorro 
-    apellido-nombre
-    saldo.
-a continuacion se ingresa los siguientes datos con los movimientos del mes:
-    numero de cuenta de ahorro
-    codigo de operacion (1 deposito / 2 extraccion)
-    importe
-
-    Confeccionar un programa que 
-        a)Actualice el archivo saldante con los movieminetos registrados
-        b) informe las cuentas que en el mes no han tenido movimiento
-        con sus respectivos saldos
-        c) informe las cuentas que en el mes han realizado mas de 5 
-        extracciones
-
-*/ 
