20f4b79c4316ee2ce450de56ae4de10416347c12
Sebastián Martínez <70720135+AS1508@users.noreply.github.com>
2023-10-11 21:32:13 -0300

    ClassSession(11/10)

--- PEB/U4/4_1.c
+++ PEB/U4/4_1.c
@@ -20,7 +20,8 @@ int main(){
     struct Promedio promedio;
     FILE*archivo;
     FILE*archi2;
-    int canalAnterior, masVisto, menosR, maximoR;
+    int contador, canalAnterior, masVisto, menosR, maximoR, maximoC;
+    float total=0;
 
     archivo=fopen(RATING, "rb");
     archi2=fopen(PROMEDIO, "wb");
@@ -29,12 +30,28 @@ int main(){
 
     fread(&ratings, sizeof(struct Datos),1, archivo);
     while(!feof(archivo)){
+        contador=0;
         canalAnterior=ratings.canal;
         while(!feof(archivo) && canalAnterior==ratings.canal){
-            if(ratings.rating<15){menosR=ratings.rating;}
-            if()
+            total+=ratings.rating;
+            contador++;
+            }
+            if(ratings.rating<15){printf("nro canal y total")} //completar
+            if(b=0 || maximoR>ratings.rating){
+                b=1;
+                maximoC=ratings.canal;
+                maximoR=total;
+                }
+            if(contador!=0){
+                promedio.promedio=total/contador;
+                promedio.canal=canalAnterior;
+                fwrite(&promedio, sizeof(struct Promedio),1, archi2)
+            }
         }
+    printf("menos rating es");  //completar
 
-    }
+    fclose(archivo);
+    fclose(archi2);
+
     return 0;
 }
---
+++ PEB/U4/4_3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define F_LLAMADAS "Llamadas.dat"
+#define F_COSTOS "Costos.dat"
+#define F_GASTOS "Gastos.dat"
+#define F_SECTOR "Sector.dat"
+
+struct Llamadas{
+//hacer
+};
+struct Costos{
+//hacer
+};
+struct Gasto
+{
+//hacer
+};
+struct Sector
+{
+//hacer
+};
+
+void Listar();
+ 
+int main(){
+    
+    return 0;
+}
