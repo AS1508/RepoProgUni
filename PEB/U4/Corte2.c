feff2d4557c657da0dc3bab0520a959f7a7dfd51
Sebastián Martínez <70720135+AS1508@users.noreply.github.com>
2023-10-23 16:27:05 -0300

    upd

--- PEB/U4/Corte2.c
+++ PEB/U4/Corte2.c
@@ -1,1 +1,36 @@
-//punto 4_4
+//punto 4_2
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define F_VENTAS "ventas.dat"
+#define F_PRODUCTO "productos.dat"
+#define F_PEDIDOS "pedidos.dat"
+
+struct Sucursal{
+    char sucursal[15];
+    int codigoSucursal;
+    int cantidad;
+};
+struct Producto
+{
+    int codigoProducto;
+    char descripcion[20];
+    float precio;
+    int stock;
+    int puntoPedido;
+    int cantidadPedido;
+};
+
+ 
+int main(){
+    FILE*archivoVenta,*archivoProducto,*archivoPedido;
+    struct Sucursal ventas;
+    struct Producto productos[200];
+    int contador=0, ventasTotal=0, sucursalMayor, cantidadMaxima;
+    float importeVentas;
+
+    
+
+    return 0;
+}
