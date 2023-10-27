90c9ea5d54cd406c9fc4fe76c5d7e7a51614cb48
Sebastián Martínez <sebymartinez70@gmail.com>
2023-10-27 00:03:55 -0300

    ModeloFinalPI

--- ProgInic/Repaso Final/FinalPasado.c
+++ ProgInic/Repaso Final/FinalPasado.c
@@ -14,7 +14,7 @@ void Objetivo(int[]);
 void VentaTrimestral(int[][MES]);
 
 int main(){
-    int venta[SUCURSALES][MES], sucursal, mes, cantidad=0, cantidadVentas=0, objetivoMesual[MES]={0};
+    int venta[SUCURSALES][MES]={{0}}, sucursal, mes, cantidad=0, cantidadVentas=0, objetivoMesual[MES]={0};
     char objetivo;
 
     cantidadVentas = Carga(venta, sucursal, mes, cantidad, objetivo, objetivoMesual);
@@ -30,17 +30,18 @@ int Carga(int ventas[][MES], int sucursal, int mes, int cantidad, char objetivo,
     int cantidadFinal=0;
 
     printf("-Sucursal: ");
-    sucursal=LeeYValida(1000, 1010, 0);
+    sucursal=LeeYValida(999, 1010, 0);
     while(sucursal!=0){
         do{    
             printf("-Mes: ");
             mes=Ingreso(1, MES);
             if(ventas[sucursal-1000][mes-1]!=0)
                 printf("Dato ya registrado.\n");
-        }while(ventas[sucursal][mes]!=0);
+        }while(ventas[sucursal-1000][mes-1]!=0);
         printf("-Cantidad: ");
         cantidad=Ingreso(1, 100000);
         printf("Cumple con el Objetivo?(S/N)\n");
+        fflush(stdin);
         scanf("%c", &objetivo);
         objetivo=toupper(objetivo);
         if(objetivo=='S')
@@ -50,40 +51,42 @@ int Carga(int ventas[][MES], int sucursal, int mes, int cantidad, char objetivo,
         cantidadFinal++;
 
         printf("-Sucursal: ");
-        sucursal=LeeYValida(1000, 1010, 0);
+        sucursal=LeeYValida(999, 1010, 0);
     }
     return cantidadFinal;
 }
 int LeeYValida(int limInf, int limSup, int fin){
     int numero;
     do{
-        scanf("%d", numero);
-    }while((numero<limInf || numero>limSup) || numero!=0);
+        scanf("%d", &numero);
+    }while((numero<=limInf || numero>limSup) && numero!=fin);
     return numero;
 }
 int Ingreso(int limInf, int limSup){
     int numero;
     do{
-        scanf("%d", numero);
+        scanf("%d", &numero);
     }while(numero<limInf || numero>limSup);
     return numero;
 }
-void Resumen(int ventas[][MES], int cantidad){
+void Resumen(int ventas[][MES]){
+    printf("Sucursal");
     for(int i=0; i<MES; i++)
-        printf("Sucursal\tMes%d\t", i+1);
+        printf("\tMes%d", i+1);
 
     for(int i=0; i<SUCURSALES; i++){
-        printf("%d", i+1000);
+        printf("\n%d", i+1000);
         for(int j=0; j<MES; j++)
-            printf("\t\t\t|%d|\t", ventas[i][j]);
+            printf("\t    |%d|", ventas[i][j]);
         printf("\n");
     }
 }
 void Objetivo(int objetivos[]){
     printf("\n\tObjetivos\n");
-    for(int i=0; i<MES; i++)
-        if(objetivos[i]<0)
+    for(int i=0; i<MES; i++){
+            if(objetivos[i]>0)
             printf("mes %d | sucursales: %d \n", i+1, objetivos[i]);
+    }
 }
 void VentaTrimestral(int ventas[][MES]){
     int suma=0;
